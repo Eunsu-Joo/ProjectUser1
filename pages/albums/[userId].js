c93614cb044a7816044b7ee1693e23ad251430c1
Eunsu-Joo <holicholicpop@gmail.com>
2021-12-07 19:42:06 +0900

    ➕ pages 컴포넌트 추가
    - params가 있는 경우 url 'posts/1'인 경우 posts/[userId].js
    - 그냥 경로 인경우 signup.js
    -  params가 있어서 디렉토리에 페이지 넣은 페이지 => posts, todos, edit, albums
    -  params 없는 페이지 signup.js

---
+++ pages/albums/[userId].js
@@ -0,0 +1,5 @@
+import Layout from "@/components/Layout";
+
+export default function Albums() {
+  return <Layout>Albums</Layout>;
+}
---
+++ pages/signup.js
@@ -0,0 +1,5 @@
+import Layout from "@/components/Layout";
+
+export default function signup() {
+  return <Layout>sign up</Layout>;
+}
